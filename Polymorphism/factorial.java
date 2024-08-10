751edb76b876cc66810782ce32ae0f726103625e
sanjanakulkarni <sanjukulkarni2001@gmail.com>
2024-08-10 12:50:25 +0530

    factorial

---
+++ Polymorphism/factorial.java
@@ -0,0 +1,13 @@
+package Polymorphism;
+
+public class factorial {
+    public static void main(String[] args) {
+        int num=5;
+        int k=1;
+        for(int i=1;i<=num;i++){
+            k =k* i;
+        }
+        System.out.println("given factorial" + num +" number is: " +  k);
+    }
+
+}
